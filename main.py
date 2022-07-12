f4dcb1038084a02a8590aea237ff10fbeba17f42
kalystote <105145441+kalystote@users.noreply.github.com>
2022-07-12 14:56:26 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -10,53 +10,7 @@
 
 import sqlite3
 import package
-from package import requetesql
-
-connection = sqlite3.connect("./plantes.db")
-cursor = connection.cursor()
-tamponplantes = []
-
-
-def maj_bdd():
-    """ajout à la bdd"""
-    cursor.executemany("""
-    INSERT INTO plantes (nom, taille, typedesol) VALUES (?, ?, ?)""", tamponplantes)
-    connection.commit()
-
-class Plante:
-    """création structure pour bdd de plantes"""
-    def __init__(self):
-        self.nom = ""
-        self.taille = 0
-        self.typesol = ""
-        self.arrosage = ""
-        self.associations = ""
-        self.Datesemis = ""
-        self.Temperaturegermination = ""
-
-    "création méthode d'ajout d'un nom de plante"
-
-Planteajoutee = Plante()
-
-def ajoutplante():
-    reponse="O"
-    while reponse == "O":
-        Planteajoutee.nom = input("Quel est le nom de la plante ? ")
-        Planteajoutee.taille = input("Quelle est sa taille ? " )
-        Planteajoutee.typesol = input("Dans quel type de sol peut-on la planter ? ")
-        tamponplantes.append((Planteajoutee.nom, Planteajoutee.taille, Planteajoutee.typesol))
-        reponse = input("Faut-il ajouter des plantes (O/N) ? ")
-
-    
-
-def affichageplante():
-    #print("le nom de la plante est ", Planteajoutee.nom, " et sa taille est de ", str(Planteajoutee.taille), '\n')
-    #print("le type de sol pour cette plante est le suivant : ", Planteajoutee.typesol, '\n')
-    print("Les plantes encodées jusqu'à présent sont les suivantes :\n")
-    for indexNom in range(len(tamponplantes)):
-        print("le nom de la plante est ", tamponplantes[indexNom][0], " et sa taille est de ", str(tamponplantes[indexNom][1]), ' cm\n')
-        print("le type de sol pour cette plante est le suivant : ", tamponplantes[indexNom][2], '\n')
-
+from package import *
 
 'Créer une variable pour le menu (sortir de l appli ou pas)'
 reponsemenu=""
@@ -76,15 +30,14 @@ while reponsemenu != "Q":
     print("Q Quitter")
     reponsemenu = input("Quel est votre choix ? ")
     if reponsemenu == "1":
-        ajoutplante()
+        fonctions.ajoutplante()
     elif reponsemenu == "2":
-        affichageplante()
+        fonctions.affichageplante()
     elif reponsemenu == "3":
-        maj_bdd()
+        requetesql.maj_bdd(fonctions.tamponplantes)
     elif reponsemenu == "4":
         print(requetesql.affichercontenubdd())
     elif reponsemenu == "Q":
         break
 
 # fin de programme main
-connection.close()
